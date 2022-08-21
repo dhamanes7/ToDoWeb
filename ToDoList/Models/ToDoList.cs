f3486efe55a322b5f210664cb6b63d412e796b24
Shubham Dhamane <105388746+dhamanes7@users.noreply.github.com>
2022-08-20 22:02:05 -0400

    Window pc connection

---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,31 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string IsCompleted { get; set; }
+
+        public string Completed { get; set; }
+
+        public ToDoList()
+        {
+            Id = 0;
+            Title = "Default";
+            IsCompleted = "NA";
+            Completed = "NA";
+        }
+
+        public ToDoList(int id, string title, string isCompleted, string completed)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = isCompleted;
+            Completed = completed;
+        }
+    }
+
+
+
+}

0c9bcdcd05c158ae9bd290686cee44a0a8bab6c1
Shubham Dhamane <105388746+dhamanes7@users.noreply.github.com>
2022-08-20 22:29:03 -0400

    controller and view added

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Models.ToDoList> toDo = new List<Models.ToDoList>();
+
+            return View("Index", toDo);
+        }
+    }
+}
